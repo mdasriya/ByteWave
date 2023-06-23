5c1b72bc5fd70098eea1553e11363965c2c06ce6
Mukesh Dasriya <dasriyamukesh87@gmail.com>
2023-06-23 15:52:17 +0530

    voice work done

--- frontend/src/components/VoiceInput.jsx
+++ frontend/src/components/VoiceInput.jsx
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './InputBox.css'
+import { AiOutlineAudio } from 'react-icons/ai';
+
 const VoiceInput = () => {
+  const [question, setQuestion] = useState('Loading...')
+
+  const handlevoice = () => {
+    if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
+      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      const recognition = new SpeechRecognition();
+      recognition.lang = 'en-US' || "hi-IN"; // Language setting
+      // Event handlers for recognition start and result
+      recognition.onstart = function () {
+        console.log('Speech recognition started');
+      }
+      recognition.onresult = function (event) {
+        const result = event.results[0][0].transcript;
+        console.log('Speech recognition result:', result);
+
+        // Update the result element with the recognized speech
+        setQuestion(result);
+      };
+      recognition.start();
+
+    } else {
+      {
+        alert('Speech recognition is not supported in this browser.');
+      }
+    }
+  }
+
+
   return (
-    <div className="input-box-container">
-      <input className="input-box" type="text" placeholder="Type something..." />
-      <div className="mic-icon">
-        <AiOutlineAudio />
+    <>
+      <div className="input-box-container">
+        <input className="input-box" type="text" placeholder="Type something..." />
+        <div className="mic-icon">
+          <AiOutlineAudio onClick={handlevoice} />
+        </div>
+        <button className='submit'>submit</button>
       </div>
-    </div>
+      <div className='question-box'>
+      <p>{question}</p>
+      </div>
+    </>
   )
 }
 
